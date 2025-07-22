b55635f9549b9eeef3c3403895e26bf44bf85df1
mdiannna <marusicdiana@gmail.com>
2025-07-22 10:23:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+from fastapi import Request
+
+app = FastAPI()
+templates = Jinja2Templates("templates")
+
+
+UPLOAD_FOLDER = 'uploads/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/uploads", StaticFiles(directory=UPLOAD_FOLDER), name="uploads")
+
+
+
+
+@app.get("/")
+async def read_root(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
